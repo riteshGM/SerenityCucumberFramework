1044c90f37ce8e9ea6c16ce9f4d36b66f2b43f05
riteshGM <riteshmansukhani@gmail.com>
2024-04-14 09:00:15 +0530

    Modified Maven Settings to Include Latest Libraries

--- src/main/java/com/serenity/pages/GoogleSearchPage.java
+++ src/main/java/com/serenity/pages/GoogleSearchPage.java
@@ -5,9 +5,10 @@ import org.openqa.selenium.Keys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.annotations.Step;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
+import net.serenitybdd.core.annotations.findby.FindBy;
 
 public class GoogleSearchPage extends PageObject{
 	private static final Logger log = LoggerFactory.getLogger(GoogleSearchPage.class);
@@ -18,21 +19,25 @@ public class GoogleSearchPage extends PageObject{
 	@FindBy(name ="q")
 	WebElementFacade enterSearch;
 	
+	@Step
 	public void launchApp() {
 		log.info("Launching the application.");
 		open();
 		getDriver().manage().window().maximize();
 	}
 	
+	@Step
 	public void enterSearchTxt(String searchKey) {
 		log.info("Entering Search Key "+searchKey);
 		searchTB.sendKeys(searchKey);
 	}
 	
+	@Step("Click Search Button")
 	public void clickSearchBtn() {
 		enterSearch.sendKeys(Keys.ENTER);
 	}
 
+	@Step
 	public void checkPageContainsText(String pageText) {
 		Assert.assertTrue("Page Do not Contain Given Message",getDriver().getPageSource().contains(pageText));
 	}
